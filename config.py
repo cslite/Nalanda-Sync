b3f6f6a78db63190d10c3d3f38ab951c9304cdf6
Tussank Gupta <34658024+cslite@users.noreply.github.com>
2019-09-18 01:26:45 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+user = "f201XXXXX@pilani.bits-pilani.ac.in"
+pwd = "PUT_YOUR_PASSWORD_HERE"
+
+
+base_url = "https://nalanda.bits-pilani.ac.in"
+plogin_url = "/login/index.php"
+pdash_url = "/my/"
+presource_url = "/mod/resource/view.php?id="
+base_path = "BITS-Acads"
+
